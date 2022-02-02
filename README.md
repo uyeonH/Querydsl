c88e0af4c73402eb388787f130ad0ce577fbf847
uyeonH <yooyeon0508@gmail.com>
2022-02-02 22:38:51 +0900

    순수JPA, 동적 쿼리를 활용한 조회 API 구현

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Tuple은 Querydsl에 종속적인 타입이다. 다른 계층으로 전달할 때는 dto 등으로 전달해야 종속적이지 않은 구조를 만들 수 있다.
 벌크연산 후 영속성컨텍스트 비우는 작업
+
+null "" 체크하기
+StringUtils.hasText
--- src/main/java/com/study/querydsl/QuerydslApplication.java
+++ src/main/java/com/study/querydsl/QuerydslApplication.java
@@ -2,11 +2,13 @@ package com.study.querydsl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
 
